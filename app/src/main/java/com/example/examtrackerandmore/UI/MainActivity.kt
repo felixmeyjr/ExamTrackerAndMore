a76936f4f61544898e6040bebe9871767a9fe452
Felix <felix.meyer97@web.de>
2021-01-25 17:42:22 +0100

    added nav_graph.xml and all the related connection part

--- app/src/main/java/com/example/examtrackerandmore/UI/MainActivity.kt
+++ app/src/main/java/com/example/examtrackerandmore/UI/MainActivity.kt
@@ -1,29 +1,17 @@
-package com.example.examtrackerandmore
+package com.example.examtrackerandmore.UI
 
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.annotation.RequiresApi
+import com.example.examtrackerandmore.R
+import com.example.examtrackerandmore.currentDate
 import java.util.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.O) // this line was necessary for currentDate function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Set the current date on top with currentDate function
-        println(currentDate())
-        currentDateTxtView.text = currentDate()
-
-
-        // Define action of Button with setOnClickListener
-
-
-
-
     }
 }
---
+++ app/src/main/java/com/example/examtrackerandmore/UI/exams/ExamsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.examtrackerandmore.UI.exams
+
+import androidx.fragment.app.Fragment
+import com.example.examtrackerandmore.R
+
+// Add Logic for fragment_exams_overview here
+class ExamsFragment : Fragment(R.layout.fragment_exams_overview) {
+}
