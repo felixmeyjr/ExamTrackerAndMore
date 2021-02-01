5d6d426ac56f1024b759291edf40cf39fb1f9c8c
Felix <felix.meyer97@web.de>
2021-02-01 19:32:57 +0100

    finished tut #10, swipe to delete and UNDO snackbar

--- app/src/main/java/com/example/examtrackerandmore/UI/exams/ExamViewModel.kt
+++ app/src/main/java/com/example/examtrackerandmore/UI/exams/ExamViewModel.kt
@@ -4,9 +4,12 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.examtrackerandmore.data.Exam
 import com.example.examtrackerandmore.data.ExamDataAccessObject
 import com.example.examtrackerandmore.data.PreferencesManager
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 /**
@@ -22,9 +25,33 @@ class ExamViewModel @ViewModelInject constructor(
 
     val preferencesFlow = preferencesManager.preferencesFlow
 
+    // Event Channel
+    private val examsEventChannel = Channel<ExamsEvent>()
+    val examsEvent = examsEventChannel.receiveAsFlow()
+
     // Updating the preference here
     fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
         preferencesManager.updateHideCompleted(hideCompleted)
+    }
+
+    // Swipe to delete
+    fun onExamSwiped(exam: Exam) = viewModelScope.launch {
+        examDataAccessObject.delete(exam)
+
+        // ViewModel is responsible for showing the snackbar window with UNDO option; only fragment
+        // or activity can show snackbar
+        // Channel: to send data between Coroutines
+        examsEventChannel.send(ExamsEvent.ShowUndoDeleteExamMessage(exam))
+    }
+
+    fun onUndoDeleteClick(exam: Exam) = viewModelScope.launch {
+        examDataAccessObject.insert(exam)
+    }
+
+    // Handle events for fragments (snackbar related); Belongs to ExamFragment, ExamEvent and ExamViewModel
+    sealed class ExamsEvent {
+        data class ShowUndoDeleteExamMessage(val exam: Exam) : ExamsEvent() // best practice to do this like this
+
     }
 
 
--- app/src/main/java/com/example/examtrackerandmore/UI/exams/ExamsFragment.kt
+++ app/src/main/java/com/example/examtrackerandmore/UI/exams/ExamsFragment.kt
@@ -8,11 +8,15 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.examtrackerandmore.R
 import com.example.examtrackerandmore.data.Exam
 import com.example.examtrackerandmore.databinding.FragmentExamsOverviewBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
@@ -42,6 +46,28 @@ class ExamsFragment : Fragment(R.layout.fragment_exams_overview) {
                 layoutManager = LinearLayoutManager(requireContext())
                 setHasFixedSize(true)
 
+                // #10 Swipe to delete
+                // DragDirs: drag and drop (vertical)
+                // SwipeDirs: Left and right (delete for example)
+                ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+                    0,
+                    ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                ) {
+                    override fun onMove(
+                        recyclerView: RecyclerView,
+                        viewHolder: RecyclerView.ViewHolder,
+                        target: RecyclerView.ViewHolder
+                    ): Boolean {
+                        return false
+                    }
+
+                    // swipe function; viewholder = current Item that should be moved
+                    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                        // Get current item at position of viewholder
+                        val exam = examAdapter.currentList[viewHolder.adapterPosition]
+                        viewModel.onExamSwiped(exam)
+                    }
+                }).attachToRecyclerView(recyclerViewExams)
             }
         }
 
@@ -50,6 +76,24 @@ class ExamsFragment : Fragment(R.layout.fragment_exams_overview) {
 
 
             examAdapter.submitList(it)
+        }
+
+        // Flow can only be selected from Coroutine -> dont block UI thread
+        // Can be done way more easier -> Handling in fragment
+        // Show snackbar with UNDO option on UI
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.examsEvent.collect { event ->
+                // Get stream of values: examsEventObject
+                when(event) {
+                    is ExamViewModel.ExamsEvent.ShowUndoDeleteExamMessage -> {
+                        Snackbar.make(requireView(), "Exam deleted", Snackbar.LENGTH_LONG)
+                            .setAction("UNDO") {
+                                // add item back to db
+                                viewModel.onUndoDeleteClick(event.exam)
+                            }.show()
+                    }
+                }
+            }
         }
 
         // Activate options menu when creating
@@ -69,7 +113,7 @@ class ExamsFragment : Fragment(R.layout.fragment_exams_overview) {
 
     // Updates fragment with OptionItem
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_hide_finished_exams -> {
                 item.isChecked = !item.isChecked
                 viewModel.onHideCompletedClick(item.isChecked)
